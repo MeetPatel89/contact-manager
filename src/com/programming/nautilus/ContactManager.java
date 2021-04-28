2830253f512ad0df76c70f2d05c4d4de4db9e928
Meet Patel <meetaiims@yahoo.com>
2021-04-28 15:34:24 -0500

    add class ContactManager to construct a map of contacts

--- src/com/programming/nautilus/ContactManager.java
+++ src/com/programming/nautilus/ContactManager.java
@@ -1,5 +1,37 @@
 package com.programming.nautilus;
 
-public class ContactManager {
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class ContactManager {
+	
+	Map<String, Contact> contactList = new ConcurrentHashMap<>();
+	
+	public void addContact(String firstName, String lastName, String phoneNumber) {
+		Contact contact = new Contact(firstName, lastName, phoneNumber);
+		validateContact(contact);
+		checkIfContactAlreadyExists(contact);
+		contactList.put(generateKey(contact), contact);
+		
+	}
+	
+	public Collection<Contact> getAllContacts() {	
+		return contactList.values();
+	}
+	
+	private void checkIfContactAlreadyExists(Contact contact) {
+		if (contactList.containsKey(generateKey(contact)))
+			throw new RuntimeException("Contact Already Exists");
+	}
+	
+	private void validateContact(Contact contact) {
+		contact.validateFirstName();
+		contact.validateLastName();
+		contact.validatePhoneNumber();
+	}
+	
+	private String generateKey(Contact contact) {
+		return String.format("%s%s", contact.getFirstName(), contact.getLastName());
+	}	
 }
